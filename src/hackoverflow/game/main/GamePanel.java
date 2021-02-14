44a5b07dc21d429c1835e48b9e2662d31ea010ba
kayrux <skusjanto@outlook.com>
2021-02-13 20:08:35 -0700

    hi

--- src/hackoverflow/game/main/GamePanel.java
+++ src/hackoverflow/game/main/GamePanel.java
@@ -2,6 +2,7 @@
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -9,18 +10,22 @@ import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 public class GamePanel extends javax.swing.JPanel implements ActionListener {
 
 	ArrayList<Wall> walls = new ArrayList<>();
 	Player player;
 	int cameraX; //create camera
 	Timer gameTimer;
+	ImageIcon lvl1Png;
 	
 	public GamePanel() {
 		
 		player = new Player(100, 400, this);
 		
-
 		reset(); //Intitalize walls with reset for ease
 
 		gameTimer = new Timer();
@@ -84,6 +89,7 @@ public class GamePanel extends javax.swing.JPanel implements ActionListener {
 		Graphics2D gtd = (Graphics2D) g;
 		
 		player.draw(gtd);
+		
 		for(Wall wall: walls) wall.draw(gtd);
 	}
 	
--- src/hackoverflow/game/main/MainFrame.java
+++ src/hackoverflow/game/main/MainFrame.java
@@ -1,16 +1,25 @@
  package hackoverflow.game.main;
 import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public class MainFrame extends javax.swing.JFrame {
-
 	public MainFrame() {
 		GamePanel panel = new GamePanel();
 		panel.setLocation(0, 0);
 		panel.setSize(this.getSize());
 		panel.setBackground(Color.LIGHT_GRAY);
 		panel.setVisible(true);
+		
+		//this.setContentPane(lblBackground1);
 		this.add(panel);
 		addKeyListener(new KeyChecker(panel));
+		
+		
+		
 	}
 	
 }

1a1624453a06575e1ee6f5c291e0219524fa0ec2
TheAlexTanasescu <alskiboy@yahoo.com>
2021-02-13 18:15:12 -0700

    Bruh

--- src/hackoverflow/game/main/Player.java
+++ src/hackoverflow/game/main/Player.java
@@ -1,4 +1,5 @@
 package hackoverflow.game.main;
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
@@ -135,7 +136,7 @@ public class Player {
 	}
 	
 	public void draw(Graphics2D gtd) {
-		//gtd.setColor(Color.BLACK);
+		//gtd.setColor(Color.GRAY);
 		//gtd.fillRect(x, y, width, height);
 		lblPlayer.getIcon().paintIcon(panel, gtd, x, y);
 		

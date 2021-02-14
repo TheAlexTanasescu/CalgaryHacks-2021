4dd6d312147c79ad3b3944c2568414abb403ac2b
kayrux <skusjanto@outlook.com>
2021-02-14 03:42:42 -0700

    bear

---
+++ src/hackoverflow/game/main/StartPanel.java
@@ -0,0 +1,17 @@
+package hackoverflow.game.main;
+
+public class StartPanel extends javax.swing.JPanel {
+	boolean isFinished;
+	
+	public StartPanel() {
+		
+	}
+	
+	
+	
+	
+	
+	public boolean isFinished() {
+		return isFinished; 
+	}
+}

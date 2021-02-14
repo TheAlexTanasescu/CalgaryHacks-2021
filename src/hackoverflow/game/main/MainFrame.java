6b2b609a292401e2f804d62c7e15fe57159dca33
Ramez <ramezhalasah@gmail.com>
2021-02-14 06:27:08 +0200

    MAPLECHAN IS HERE :D -Ramez

--- src/hackoverflow/game/main/MainFrame.java
+++ src/hackoverflow/game/main/MainFrame.java
@@ -1,9 +1,15 @@
  package hackoverflow.game.main;
 import java.awt.Color;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.tools.Tool;
 
 public class MainFrame extends javax.swing.JFrame {
 
 	public MainFrame() {
+		
 		GamePanel panel = new GamePanel();
 		panel.setLocation(0, 0);
 		panel.setSize(this.getSize());
@@ -11,6 +17,9 @@ public class MainFrame extends javax.swing.JFrame {
 		panel.setVisible(true);
 		this.add(panel);
 		addKeyListener(new KeyChecker(panel));
+		
+		
+
 	}
 	
 }

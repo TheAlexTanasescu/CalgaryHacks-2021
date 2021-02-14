d397e09a17ca4e2a89f58f6da962debadb22d7b5
Ramez <ramezhalasah@gmail.com>
2021-02-14 08:56:48 +0200

    added the beautiful useless justin beaver class -Ramez~

---
+++ src/GameMob/Beaver.java
@@ -0,0 +1,18 @@
+package GameMob;
+
+import hackoverflow.game.main.GamePanel;
+
+public class Beaver extends Mob{
+
+	public Beaver(String name, int x, int y, GamePanel panel, String mobImgPath) {
+		super(name, x, y, panel, mobImgPath);
+		// TODO Auto-generated constructor stub
+	}
+		
+		
+}
+	
+	
+	
+	
+
